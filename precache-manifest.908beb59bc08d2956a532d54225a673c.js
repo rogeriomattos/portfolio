370fa5d833166030d4d63171a206f72a7ec57a38
rogerio.mattos7 <rogeriomattos38@outlook.com>
2020-02-19 22:38:02 -0300

    Updates

---
+++ precache-manifest.908beb59bc08d2956a532d54225a673c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e21b6ec9e3fd397fe3877776437a79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a44c421ace1b98baf3d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad8e8dccf92391a62d32",
+    "url": "/static/js/2.50fdd86e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.50fdd86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a44c421ace1b98baf3d",
+    "url": "/static/js/main.f094cba1.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
